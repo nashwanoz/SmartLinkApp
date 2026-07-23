be262ecf8414de8be7efca405fd8459bb56d3b93
nashwanoz <t-3z@hotmail.com>
2026-07-23 08:34:13 +0300

    Create DeveloperActivity.java

---
+++ app/src/main/java/com/smart/link/DeveloperActivity.java
@@ -0,0 +1,14 @@
+package com.smart.link;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DeveloperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer);
+        // الشاشة تعرض فقط البيانات النصية التي قمنا بإعدادها في ملف XML الخاص بك
+    }
+}
