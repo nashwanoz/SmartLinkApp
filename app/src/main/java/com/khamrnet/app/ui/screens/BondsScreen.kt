ad24bd3aef5f2355487bc1866cf360cdf122db7e
nashwanoz <t-3z@hotmail.com>
2026-08-22 08:35:02 +0300

    Update BondsScreen.kt

--- app/src/main/java/com/khamrnet/app/ui/screens/BondsScreen.kt
+++ app/src/main/java/com/khamrnet/app/ui/screens/BondsScreen.kt
@@ -3,6 +3,7 @@ package com.khamrnet.app.ui.screens
 import android.widget.Toast
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
